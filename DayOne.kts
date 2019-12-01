a8582f54de922f50ba19e25ce1858d709fac823c
LitenApe <sonvo95@outlook.com>
2019-12-01 16:30:16 +0100

    feat: Add solution for day one

---
+++ DayOne.kts
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun calcFuel(value: Int): Int
+  = (value / 3) - 2
+
+fun calcFuelRec(value: Int): Int
+  = calcFuel(value).let { if (it <= 0) 0 else it + calcFuelRec(it) }
+
+fun solve(inputs: List<Int>, func: (value: Int) -> Int): Int
+  = inputs.fold(0) { acc, it -> acc + func(it) }
+
+fun readFile(filename: String): List<Int>
+  = File(filename)
+    .readLines()
+    .map { it.toInt() }
+
+val inputs = readFile("./data/dayOne.txt")
+println("Part One: " + solve(inputs, ::calcFuel)) // => 3399394
+println("Part Two: " + solve(inputs, ::calcFuelRec)) // => 5096223
