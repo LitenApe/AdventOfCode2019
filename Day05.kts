919e1202634f8a384cbb2ce5293f9e8da3fe1287
LitenApe <sonvo95@outlook.com>
2019-12-06 19:00:30 +0100

    fix: Minor fix for more correct code

--- Day05.kts
+++ Day05.kts
@@ -64,24 +64,27 @@ fun iz(cursor: Int, mode: String, program: MutableList<Int>): Int {
   return if (x == 0) addr else cursor + 3
 }
 
-fun lt(cursor: Int, mode: String, program: MutableList<Int>): Int {
+fun lt(cursor: Int, mode: String, program: MutableList<Int>): MutableList<Int> {
   val x = getValue(program.get(cursor + 1), mode.get(1), program)
   val y = getValue(program.get(cursor + 2), mode.get(0), program)
   val addr = program.get(cursor + 3)
   println("LSS: $x < $y -> $addr")
-  return if ( x < y ) addr else cursor + 4
+  program.set(addr, if (x < y ) 1 else 0)
+  return program
 }
 
-fun eq(cursor: Int, mode: String, program: MutableList<Int>): Int {
+fun eq(cursor: Int, mode: String, program: MutableList<Int>): MutableList<Int> {
   val x = getValue(program.get(cursor + 1), mode.get(1), program)
   val y = getValue(program.get(cursor + 2), mode.get(0), program)
   val addr = program.get(cursor + 3)
-  println("EQL: $x == $y -> addr")
-  return if ( x == y ) addr else cursor + 4
+  println("EQL: $x == $y -> $addr")
+  program.set(addr, if (x == y ) 1 else 0)
+  return program
 }
 
 fun computer(program: MutableList<Int>, cursor: Int = 0, input: Int = 5) {
   val (opCode, mode) = getCommand(program.get(cursor))
+  println(program)
   when (opCode) {
     99 -> return
     1 -> return computer(add(cursor, mode, program), cursor + 4)
@@ -90,8 +93,8 @@ fun computer(program: MutableList<Int>, cursor: Int = 0, input: Int = 5) {
     4 -> return computer(dbg(cursor, mode, program), cursor + 2)
     5 -> return computer(program, nz(cursor, mode, program))
     6 -> return computer(program, iz(cursor, mode, program))
-    7 -> return computer(program, lt(cursor, mode, program))
-    8 -> return computer(program, eq(cursor, mode, program))
+    7 -> return computer(lt(cursor, mode, program), cursor + 4)
+    8 -> return computer(eq(cursor, mode, program), cursor + 4)
     else -> throw Error("Shit went ham!")
   }
 }
@@ -112,4 +115,4 @@ computer(inputs)
 // computer(listOf(3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9))
 // computer(listOf(3,3,1105,-1,9,1101,0,0,12,4,12,99,1))
 
-// computer(listOf(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99))
+// computer(listOf(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99).toMutableList())
